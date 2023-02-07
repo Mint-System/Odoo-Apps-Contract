97b14f69634ba15b32ecfe051223774266e68f81
janikvonrotz <contact@janikvonrotz.ch>
2023-02-07 08:55:44 +0100

    [contract_format_date_marker] additional check

--- contract_format_date_marker/models/contract_line.py
+++ contract_format_date_marker/models/contract_line.py
@@ -9,17 +9,18 @@ class ContractLine(models.Model):
     
     def _insert_markers(self, first_date_invoiced, last_date_invoiced):
         """Append first and last date."""
-        res = super()._insert_markers(first_date_invoiced, last_date_invoiced)
 
+        name = self.name
         if first_date_invoiced and last_date_invoiced:
+            name = super()._insert_markers(first_date_invoiced, last_date_invoiced)
             lang_obj = self.env["res.lang"]
             lang = lang_obj.search([("code", "=", self.contract_id.partner_id.lang)], limit=1)
             if lang.code:
                 locale.setlocale(locale.LC_TIME, lang.code + '.utf8')
                         
             date_format = '%B %Y'
-            res += "\n"
-            res += first_date_invoiced.strftime(date_format) + " - " + last_date_invoiced.strftime(date_format)
+            name += "\n"
+            name += first_date_invoiced.strftime(date_format) + " - " + last_date_invoiced.strftime(date_format)
             
-        return res
+        return name
     
