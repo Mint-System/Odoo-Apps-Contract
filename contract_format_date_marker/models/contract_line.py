302acc93bbde84f7632fe3a1ff8c2daf9a26df9e
Janik von Rotz <login@janikvonrotz.ch>
2025-03-28 17:23:58 +0100

    feat(contract_format_date_marker): init

---
+++ contract_format_date_marker/models/contract_line.py
@@ -0,0 +1,30 @@
+import locale
+import logging
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class ContractLine(models.Model):
+    _inherit = "contract.line"
+
+    def _insert_markers(self, first_date_invoiced, last_date_invoiced):
+        """Append first and last date."""
+
+        name = self.name
+        if first_date_invoiced and last_date_invoiced:
+            name = super()._insert_markers(first_date_invoiced, last_date_invoiced)
+            lang_obj = self.env["res.lang"]
+            lang = lang_obj.search([("code", "=", self.contract_id.partner_id.lang)], limit=1)
+            if lang.code:
+                try:
+                    locale.setlocale(locale.LC_TIME, lang.code + ".utf8")
+                except locale.Error:
+                    pass
+
+            date_format = "%B %Y"
+            name += "\n"
+            name += first_date_invoiced.strftime(date_format) + " - " + last_date_invoiced.strftime(date_format)
+
+        return name
