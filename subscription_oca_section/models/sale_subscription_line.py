e25f103b07ab8f88d044eaabcbffcc5ffb4ad7db
Ulrich Kilian <uk@sdcience-and-more.de>
2026-07-10 10:25:09 +0200

    feat(subscription_oca_section): copy section and note lines to invoice

--- subscription_oca_section/models/sale_subscription_line.py
+++ subscription_oca_section/models/sale_subscription_line.py
@@ -16,3 +16,32 @@ class SaleSubscriptionLine(models.Model):
             ('line_note', "Note"),
         ],
         default=False)
+
+
+    def _prepare_account_move_line(self):
+        self.ensure_one()
+
+        if self.display_type:
+            return {
+                "display_type": self.display_type,
+                "name": self.name,
+                "sequence": self.sequence,
+            }
+
+        account = (
+            self.product_id.property_account_income_id
+            or self.product_id.categ_id.property_account_income_categ_id
+        )
+        return {
+            "sequence": self.sequence,
+            "product_id": self.product_id.id,
+            "name": self.name,
+            "quantity": self.product_uom_qty,
+            "price_unit": self.price_unit,
+            "discount": self.discount,
+            "price_subtotal": self.price_subtotal,
+            "tax_ids": [(6, 0, self.tax_ids.ids)],
+            "product_uom_id": self.product_id.uom_id.id,
+            "account_id": account.id,
+        }
+
